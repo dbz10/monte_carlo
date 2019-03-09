8419b9e5b6c16f819dba5fe296da2d084395b9a9
Daniel <dbzion@gmail.com>
2019-03-08 19:59:04 -0800

    ~

--- monte_carlo/main.jl
+++ monte_carlo/main.jl
@@ -1,7 +1,31 @@
-1+1
+############################################
+#
+# High level driver code for Monte carlo calculations
+#
+# Daniel Ben-Zion
+# 2019
+#
+############################################
 
-rand(Float64,10,10)
 
-1+3
+#goal:
+# what determines a model at the highest level?
 
-α
+# level zero:
+# lattice
+#   - lattice needs to know: position of each site
+#   - each site should know who its neighbors are
+#   - thats it?
+# hamiltonian
+#   - or something like that, more generally boltzmann weight
+# degrees of freedom
+#   - ising spin, rotor, etc?
+
+# what determines monte carlo?
+# temperature, number of steps, method (ie metropolis)
+
+
+
+dims = (4); # dimensions of the lattice
+lattice = ?;
+model = ?;
