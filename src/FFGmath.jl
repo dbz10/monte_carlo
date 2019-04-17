e478d5af0f3e2ee38d83a9d30282c5aa34410a09
Daniel <dbzion@gmail.com>
2019-04-17 08:51:11 -0700

    extend det_ratio_factor to generalized matrix det lemma

--- src/FFGmath.jl
+++ src/FFGmath.jl
@@ -12,9 +12,14 @@ function get_Inverse_Matrix(r,states)
     return inv(mat)
 end
 
+using LinearAlgebra
 function det_ratio_factor(A_inv,u,v)
     """ Compute ratio of determinants using Matrix Determinant Lemma"""
-    return 1+ v'*A_inv*u
+    # extension for higher rank update
+    correction = hcat(v'*A_inv*u) # turns scalar output into 1 by 1 matrix ¯\_(ツ)_/¯
+    
+    one = Matrix{Float64}(I,size(correction))
+    return det(one+ correction)
 end
 
 function sherman_morrison_inverse_update(A_inv,u,v)
